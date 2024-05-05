431ba708776f6587db0e715b426a487192b69e29
Prashant002x <prashantsharma6002002@gmail.com>
2024-05-06 02:19:19 +0530

    first commit

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from PIL import Image
 import numpy as np
 import plotly.figure_factory as ff
 import streamlit as st
-from code.DiseaseModel import DiseaseModel
+# from code.DiseaseModel import DiseaseModel
 from code.helper import prepare_symptoms_array
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -58,36 +58,36 @@ with st.sidebar:
 
 
 
-# multiple disease prediction
-if selected == 'Disease Prediction': 
-    # Create disease class and load ML model
-    disease_model = DiseaseModel()
-    disease_model.load_xgboost('model/xgboost_model.json')
+# # multiple disease prediction
+# if selected == 'Disease Prediction': 
+#     # Create disease class and load ML model
+#     disease_model = DiseaseModel()
+#     disease_model.load_xgboost('model/xgboost_model.json')
 
-    # Title
-    st.write('# Disease Prediction using Machine Learning')
+#     # Title
+#     st.write('# Disease Prediction using Machine Learning')
 
-    symptoms = st.multiselect('What are your symptoms?', options=disease_model.all_symptoms)
+#     symptoms = st.multiselect('What are your symptoms?', options=disease_model.all_symptoms)
 
-    X = prepare_symptoms_array(symptoms)
+#     X = prepare_symptoms_array(symptoms)
 
-    # Trigger XGBoost model
-    if st.button('Predict'): 
-        # Run the model with the python script
+#     # Trigger XGBoost model
+#     if st.button('Predict'): 
+#         # Run the model with the python script
         
-        prediction, prob = disease_model.predict(X)
-        st.write(f'## Disease: {prediction} with {prob*100:.2f}% probability')
+#         prediction, prob = disease_model.predict(X)
+#         st.write(f'## Disease: {prediction} with {prob*100:.2f}% probability')
 
 
-        tab1, tab2= st.tabs(["Description", "Precautions"])
+#         tab1, tab2= st.tabs(["Description", "Precautions"])
 
-        with tab1:
-            st.write(disease_model.describe_predicted_disease())
+#         with tab1:
+#             st.write(disease_model.describe_predicted_disease())
 
-        with tab2:
-            precautions = disease_model.predicted_disease_precautions()
-            for i in range(4):
-                st.write(f'{i+1}. {precautions[i]}')
+#         with tab2:
+#             precautions = disease_model.predicted_disease_precautions()
+#             for i in range(4):
+#                 st.write(f'{i+1}. {precautions[i]}')
 
 
 
